4b30877460f9be7d117216c9181080284756daf0
Varad Deshpande <varaddeshpande81@gmail.com>
2022-10-24 09:27:18 +0530

    initial commit

---
+++ 1myfirstprogram.c
@@ -0,0 +1,11 @@
+// #include<stdio.h>
+
+// int main() 
+// main - Header file library.Let us work with input & output functions like printf()
+// {
+//     printf("Hello World!"); 
+    // printf()- output or function to print.In C every line end with semicolon(;).
+    // return 0;
+    // return 0 ends the main() function.
+// }
+// code inside {code to be executed} is executed .
