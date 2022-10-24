99a54fe0e66d43e6602d4448caccc6d34d9c2fa3
Varad Deshpande <varaddeshpande81@gmail.com>
2022-10-24 10:40:33 +0530

    Comments in C

---
+++ 5comments.c
@@ -0,0 +1,16 @@
+// Comments - use dto explain code and make it more readable.Also it is used to prevent execution when testing alternative code.
+// Single Line comment - ( // ) used to give single line comment.
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello World!");
+//     // This is a single line comment .
+// }
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello World!");
+//     /*This is a multi line comment.
+//     This line gets ignored by the compiler .
+//     */
+// }
