b0c75257c6ecee054c668698d76561d74678ef32
Varad Deshpande <varaddeshpande81@gmail.com>
2022-10-24 10:39:19 +0530

    Escape Sequences

---
+++ 4escapesequences.c
@@ -0,0 +1,25 @@
+// Escape Sequences - it forces the cursor to change its position to the beginning of the next line on the screen.
+// \t - creates a horizontal tab (around 4 spaces)
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello World! \t");
+//     printf("I am learning C.");
+//     return 0;
+// }
+// \\ - Backslash character - creates a single \ (backslash) 
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello World!\\");
+//     printf("I am learning C");
+//     return 0;
+// }
+// \" - Double Quote(") Character - creates a " (double quote)
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello World! \" ");
+//     printf("I am learning C");
+//     return 0;
+// }
