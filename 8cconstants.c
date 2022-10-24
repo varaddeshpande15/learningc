610e80ce402e15c72a5a10c27fa850cae46a6bd6
Varad Deshpande <varaddeshpande81@gmail.com>
2022-10-24 10:43:57 +0530

    Constants in C

---
+++ 8cconstants.c
@@ -0,0 +1,16 @@
+// C constants - If you don’t want to change a existing variable then you can use const keyword and declare the variable as constant.
+// #include<stdio.h>
+// int main()
+// {
+//     const int myNum = 15;  // myNum will always be 15
+//     myNum = 10;  // error: assignment of read-only variable 'myNum'
+//     return 0;
+// }
+// Usually used for variables unlikely to change.Good practice to declare them with uppercase. 
+// It is not required, but useful for code readability and common for C programmers.
+// {
+//     const int minutesPerHour = 60;
+//     const float PI = 3.14;
+//     const int BIRTHYEAR = 1980;
+//     return 0;
+// }
