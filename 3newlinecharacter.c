23418ac8ae2fd2fa08b772c27dccffd67ea2d087
Varad Deshpande <varaddeshpande81@gmail.com>
2022-10-24 10:35:17 +0530

    New line character

---
+++ 3newlinecharacter.c
@@ -0,0 +1,23 @@
+// Systematic way - In two lines
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello World! \n");
+//     printf("I am learning C");
+//     return 0;
+// }
+// In single line 
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello World \nI am learning C");
+//     return 0;
+// }
+// Double \n character - creates a new line
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello World!\n\n");
+//     printf("I am learning C");
+//     return 0;
+// }
