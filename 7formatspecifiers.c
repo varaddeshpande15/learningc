dbff6d16f81c310c7e30d1280989d6bb83a68011
Varad Deshpande <varaddeshpande81@gmail.com>
2022-10-24 10:43:07 +0530

    Format Specifiers

---
+++ 7formatspecifiers.c
@@ -0,0 +1,27 @@
+// Format Specifiers - printf() does not works for variables for that we need to learn format specifiers
+// Tells the compiler what type of data the variable is storing. It isa placeholder for the variable value.
+// A format specifier starts with a percentage sign %, followed by a character.
+// %d  - For int , %f - For float , %c - For character, %s for strings 
+// #include<stdio.h>
+// int main()
+// {
+//     int myNum = 5;             // Integer (whole number)
+//     float myFloatNum = 5.99;   // Floating point number
+//     char myLetter = 'D';       // Character
+//     printf("%d\n", myNum);
+//     printf("%f\n", myFloatNum);
+//     printf("%c\n", myLetter);
+
+// }
+// C Variable Names - General Rules for naming varibles -
+// Names can contain letters, digits and underscores
+// Names must begin with a letter or an underscore (_)
+// Names are case sensitive (myVar and myvar are different variables)
+// Names cannot contain whitespaces or special characters like !, #.
+// Reserved words (such as int) cannot be used as names
+// Data Types - A variable in C must be a specified data type use format inside printf() to display it.
+// Basic Data Types in C : 
+// 1) int - 2 to 4 bytes - Stores whole nos. without decimals.
+// 2) float - 4 bytes - stores fractional nos.containing one or more decimals.Sufficient for storing 7 decimal no.
+// 3) double - 8 bytes - stores fractional nos.containing one or more decimals.Sufficient for storing 7 decimal no.
+// 4) char - 1 byte -stores single letter/character/number.
