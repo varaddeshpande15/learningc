90cc5fa9ddab8e75f728911f172ff80cc2ca84f8
Varad Deshpande <varaddeshpande81@gmail.com>
2022-10-24 10:28:00 +0530

    printf or  output function

---
+++ 2outputfunction.c
@@ -0,0 +1,15 @@
+// Print two different lines
+// #include<stdio.h>
+// int main() 
+// {
+//     printf("Hello World!");
+//     printf("I am learning C");
+//     return 0;
+// }
+// Print two different lines in one printf code
+// #include<stdio.h>
+// int main()
+// {
+//     printf("Hello World!I am learning C");
+//     return 0;
+// } 
