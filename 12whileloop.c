c858c2bd577cde8fabe9caab34c4bf16d08f170e
Varad Deshpande <varaddeshpande81@gmail.com>
2022-10-24 10:53:18 +0530

    While Loop

---
+++ 12whileloop.c
@@ -0,0 +1,34 @@
+// Loops can execute a block of code as long as a specified condition is reached.
+// It save time, reduce errors, and they make code more readable.
+// While Loop :
+// The while loop loops through a block of code as long as a specified condition is true:
+// while (condition) {
+  // code block to be executed
+// }
+// The code in the loop will run, over and over again, as long as a variable (i) is less than 5:
+// #include<stdio.h>
+// int main()
+// {
+//     int i = 0;
+// while (i < 5) {
+//   printf("%d\n", i);
+//   i++;
+// }
+// }
+// Note: Do not forget to increase the variable used in the condition (i++), otherwise the loop will never end!
+// The Do/While Loop : The do/while loop is a variant of the while loop.
+// This loop will execute the code block once, before checking if the condition is true,
+// then it will repeat the loop as long as the condition is true.
+// do {
+  // code block to be executed
+//   }
+// while (condition);
+// {
+//     int i = 0;
+// do {
+//   printf("%d\n", i);
+//   i++;
+// }
+// while (i < 5);
+// }
+// Do not forget to increase the variable used in the condition, otherwise the loop will never end!
