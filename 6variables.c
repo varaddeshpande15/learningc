29011c6e8016f23c1b10a49b01fe265bd9556df3
Varad Deshpande <varaddeshpande81@gmail.com>
2022-10-24 10:41:48 +0530

    Variables in C

---
+++ 6variables.c
@@ -0,0 +1,36 @@
+// Varibles are containers storing data values.
+// Types of variables - 1] int - integers without decimals ,2] float - decimal numbers ,
+// 3] char -stores single character and it is surrounded by ''(single quotes).
+// Declaring or Crearing a variable - type variableName = value;
+// #include<stdio.h>
+// int main()
+// {
+//     // int myNum = 15; 
+//     // OR
+//     // int myNum;
+//     // myNum=15;
+// }
+// To change value of declared variable -
+// #include<stdio.h>
+// int main()
+// {
+//     int myNum = 15;  // myNum is 15
+//     myNum = 10;  // Now myNum is 10
+// To declare multiple variable - 
+//     int x = 5, y = 6, z = 50;
+// To declare multiple variables of same values -
+//     int x, y, z;
+//     x = y = z = 50; 
+//     // To add variables -
+//     int x = 10;
+//     int y = 15;
+//     int sum = x + y ;
+// }
+// Declaring multiple variables of different values -
+// #include<stdio.h>
+// int main()
+// {
+//     int x = 5, y = 6, z = 50;
+//     return 0;
+// }
+// Declaring multiple variables of same values -
